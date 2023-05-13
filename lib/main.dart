b2a91999e9cda19c9e7543d4b8870daaf2e02cc7
svhiranx <svhiran@gmail.com>
2023-05-13 11:55:16 +0530

    Global Font

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:abhyukthafoods/pages/auth/signuppage.dart';
-import 'package:abhyukthafoods/pages/profile/favouritepage.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:abhyukthafoods/pages/auth/onboardingpage.dart';
 import 'package:flutter/material.dart';
-import 'pages/auth/onboardingpage.dart';
-import 'comps/navbar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -11,12 +8,23 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  final textTheme = GoogleFonts.dmSansTextTheme;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+            secondary: Colors.black,
+            seedColor: const Color.fromRGBO(20, 120, 70, 1)),
+        textTheme: textTheme(
+          const TextTheme(
+              bodyLarge: TextStyle(fontSize: 20),
+              titleMedium:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
+      home: const OnboardingPage(),
     );
   }
 }
