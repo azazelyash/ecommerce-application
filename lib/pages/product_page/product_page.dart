ccb25ee5f14f1a5ef64f5eeaaf165b7592ff676c
Yash Vishwakarma <jss.yash085@gmail.com>
2023-05-20 01:51:08 +0530

    Cart Logic Created, UI Remaining

--- lib/pages/product_page/product_page.dart
+++ lib/pages/product_page/product_page.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:abhyukthafoods/comps/product_card.dart';
+import 'package:abhyukthafoods/models/cart.dart';
 import 'package:abhyukthafoods/models/products.dart';
+import 'package:abhyukthafoods/models/variations.dart';
 import 'package:abhyukthafoods/network/fetch_products.dart';
 import 'package:abhyukthafoods/network/fetch_variations.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +20,7 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   final PageController pageController = PageController();
   Future<List>? variationData;
+  List<dynamic>? variationList;
 
   int varIndex = 0;
   late List<bool> isSelected = [];
@@ -26,6 +31,7 @@ class _ProductPageState extends State<ProductPage> {
       isSelected.fillRange(0, isSelected.length, false);
       isSelected[index] = true;
     });
+    getVariationId();
   }
 
   String resolvePrice(dynamic product) {
@@ -60,12 +66,15 @@ class _ProductPageState extends State<ProductPage> {
     isSelected[0] = true;
   }
 
+  Future<void> getVariationId() async {
+    variationList = await variationData;
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(widget.product.images!.length);
-    var pages = [
-      ...widget.product.images!.map((e) => Image(image: NetworkImage(e['src'])))
-    ];
+    // print(widget.product.images!.length);
+    log(varIndex.toString());
+    var pages = [...widget.product.images!.map((e) => Image(image: NetworkImage(e['src'])))];
     var theme = Theme.of(context);
     return Scaffold(
       backgroundColor: theme.primaryColor,
@@ -80,10 +89,7 @@ class _ProductPageState extends State<ProductPage> {
                   GestureDetector(
                     child: Container(
                         margin: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: theme.colorScheme.secondary,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(100))),
+                        decoration: BoxDecoration(color: theme.colorScheme.secondary, borderRadius: const BorderRadius.all(Radius.circular(100))),
                         padding: const EdgeInsets.all(10),
                         child: const Icon(
                           Icons.arrow_back,
@@ -108,19 +114,12 @@ class _ProductPageState extends State<ProductPage> {
                                   return const CircularProgressIndicator();
                                 } else {
                                   if (widget.product.variations!.isNotEmpty) {
-                                    pages = [
-                                      Image(
-                                          image: NetworkImage(snapshot
-                                              .data![varIndex].imageUrl))
-                                    ];
+                                    pages = [Image(image: NetworkImage(snapshot.data![varIndex].imageUrl))];
                                   }
                                   return PageView.builder(
                                     controller: pageController,
                                     itemCount: pages.length,
-                                    itemBuilder: (context, index) => Container(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 10),
-                                        child: pages[index]),
+                                    itemBuilder: (context, index) => Container(padding: const EdgeInsets.symmetric(horizontal: 10), child: pages[index]),
                                   );
                                 }
                               }),
@@ -128,8 +127,7 @@ class _ProductPageState extends State<ProductPage> {
                         const SizedBox(
                           height: 10,
                         ),
-                        SmoothPageIndicator(
-                            controller: pageController, count: 1),
+                        SmoothPageIndicator(controller: pageController, count: 1),
                         const SizedBox(
                           height: 10,
                         ),
@@ -144,157 +142,149 @@ class _ProductPageState extends State<ProductPage> {
               child: Container(
                 padding: const EdgeInsets.all(20),
                 width: double.infinity,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20))),
-                child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
+                decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+                child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
 //
 //---------------------------------- Title widget -----------------------------
 
-                      Row(
-                        children: [
-                          Expanded(
-                              child: Text(
-                            widget.product.name,
-                            style: theme.textTheme.titleMedium,
-                          )),
-                          GestureDetector(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: theme.colorScheme.primary,
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(5))),
-                              padding: const EdgeInsets.all(10),
-                              child: SvgPicture.asset(
-                                  color: Colors.white, 'assets/Icons/love.svg'),
-                            ),
-                            onTap: () {},
-                          ),
-                        ],
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        widget.product.name,
+                        style: theme.textTheme.titleMedium,
+                      )),
+                      GestureDetector(
+                        child: Container(
+                          decoration: BoxDecoration(color: theme.colorScheme.primary, borderRadius: const BorderRadius.all(Radius.circular(5))),
+                          padding: const EdgeInsets.all(10),
+                          child: SvgPicture.asset(color: Colors.white, 'assets/Icons/love.svg'),
+                        ),
+                        onTap: () {},
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
 
 //---------------------------------- Price and Counter -----------------------------
 
-                      FutureBuilder(
-                        future: variationData,
-                        builder: (context, snapshot) => !snapshot.hasData
-                            ? const CircularProgressIndicator()
-                            : Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(widget.product.variations!.isEmpty
-                                      ? resolvePrice(widget.product)
-                                      : resolvePrice(snapshot.data![varIndex])),
-                                  const CounterWidget()
-                                ],
-                              ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                  FutureBuilder(
+                    future: variationData,
+                    builder: (context, snapshot) => !snapshot.hasData
+                        ? const CircularProgressIndicator()
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text(widget.product.variations!.isEmpty ? resolvePrice(widget.product) : resolvePrice(snapshot.data![varIndex])), const CounterWidget()],
+                          ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
 
 //---------------------------------- Variation selector  -----------------------------
 
-                      FutureBuilder(
-                          future: variationData,
-                          builder: (context, snapshot) {
-                            if (!snapshot.hasData) {
-                              return const CircularProgressIndicator();
-                            }
-                            return Wrap(
-                              children: List<Widget>.generate(
-                                  isSelected.length,
-                                  (index) => VariationButton(
-                                        label: snapshot.data![index].name,
-                                        isSelected: isSelected[index],
-                                        selectButtonCallback: () =>
-                                            selectButton(index),
-                                      )),
-                            );
-                          }),
-// ------------------------------------------------------------------------------------
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(double.infinity, 50),
-                            backgroundColor: Colors.black),
-                        child: const Text('Add to Cart'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(double.infinity, 50),
-                        ),
-                        child: const Text('Buy Now'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Product Description',
-                        style: theme.textTheme.titleMedium
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(widget.product.shortDescription == ''
-                                ? 'No description'
-                                : widget.product.shortDescription!),
+                  FutureBuilder(
+                    future: variationData,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const CircularProgressIndicator();
+                      }
+                      return Wrap(
+                        children: List<Widget>.generate(
+                          isSelected.length,
+                          (index) => VariationButton(
+                            label: snapshot.data![index].name,
+                            isSelected: isSelected[index],
+                            selectButtonCallback: () => selectButton(index),
                           ),
-                        ],
+                        ),
+                      );
+                    },
+                  ),
+// ------------------------------------------------------------------------------------
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  /* --------------------- Add to cart and buy now buttons -------------------- */
+
+                  ElevatedButton(
+                    onPressed: () {
+                      // check if the product has variations then add the variation id to the cart
+                      if (widget.product.variations!.isNotEmpty) {
+                        log("Adding variation to cart");
+                        Cart().addItemToCart(variationList![varIndex].id, 1);
+                        Cart().printCart();
+                        return;
+                      }
+                      log("Adding product to cart");
+                      Cart().addItemToCart(widget.product.id, 1);
+                      Cart().printCart();
+                    },
+                    style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 50), backgroundColor: Colors.black),
+                    child: const Text('Add to Cart'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Text('Buy Now'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Product Description',
+                    style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(widget.product.shortDescription == '' ? 'No description' : widget.product.shortDescription!),
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Suggested Products',
-                        style: theme.textTheme.titleMedium
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      FutureBuilder(
-                        future: fetchProducts(),
-                        builder: (context, snapshot) => !snapshot.hasData
-                            ? const CircularProgressIndicator()
-                            : SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    ...snapshot.data!.map(
-                                      (e) => Container(
-                                        padding: const EdgeInsets.all(10),
-                                        height: 300,
-                                        width: 200,
-                                        child: ProductCard(
-                                          product: e,
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                )),
-                      ),
-                    ]),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Suggested Products',
+                    style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FutureBuilder(
+                    future: fetchProducts(),
+                    builder: (context, snapshot) => !snapshot.hasData
+                        ? const CircularProgressIndicator()
+                        : SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: [
+                                ...snapshot.data!.map(
+                                  (e) => Container(
+                                    padding: const EdgeInsets.all(10),
+                                    height: 300,
+                                    width: 200,
+                                    child: ProductCard(
+                                      product: e,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )),
+                  ),
+                ]),
               ),
             )
           ],
@@ -305,11 +295,7 @@ class _ProductPageState extends State<ProductPage> {
 }
 
 class VariationButton extends StatefulWidget {
-  const VariationButton(
-      {super.key,
-      required this.label,
-      required this.selectButtonCallback,
-      required this.isSelected});
+  const VariationButton({super.key, required this.label, required this.selectButtonCallback, required this.isSelected});
   final String label;
   final Function selectButtonCallback;
   final bool isSelected;
@@ -326,13 +312,10 @@ class _VariationButtonState extends State<VariationButton> {
         onPressed: () {
           widget.selectButtonCallback();
         },
-        style: ElevatedButton.styleFrom(
-            backgroundColor: widget.isSelected ? null : Colors.white,
-            minimumSize: const Size(80, 100)),
+        style: ElevatedButton.styleFrom(backgroundColor: widget.isSelected ? null : Colors.white, minimumSize: const Size(80, 100)),
         child: Text(
           widget.label,
-          style:
-              TextStyle(color: widget.isSelected ? Colors.white : Colors.black),
+          style: TextStyle(color: widget.isSelected ? Colors.white : Colors.black),
         ),
       ),
     );
