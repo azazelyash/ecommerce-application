780517181b08edb0aa5cf0fb30e262bc74523a97
Yash Vishwakarma <jss.yash085@gmail.com>
2023-05-23 02:00:02 +0530

    address logic pending

--- lib/pages/profile/address_page.dart
+++ lib/pages/profile/address_page.dart
@@ -1,4 +1,7 @@
 import 'package:abhyukthafoods/comps/appbar.dart';
+import 'package:abhyukthafoods/pages/profile/edit_address_page.dart';
+import 'package:abhyukthafoods/services/shared_services.dart';
+import 'package:abhyukthafoods/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class AddressPage extends StatefulWidget {
@@ -38,53 +41,114 @@ class AddressView extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 16,
+              height: 20,
             ),
-            const Text(
-              "2/55 Kala Kuan, Housing Board, Near Vidhya Public School, Alwar, Rajasthan, 301001",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 4,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
+              ),
+              child: FutureBuilder(
+                future: SharedService.customerDetails(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    String address = " ";
+                    // String address = "${snapshot.data!.billing['address_1']}, ${snapshot.data!.billing['address_2']}, ${snapshot.data!.billing['city']}, ${snapshot.data!.billing['state']}, ${snapshot.data!.billing['postcode']}, ${snapshot.data!.billing['country']}";
+                    return ListView(
+                      shrinkWrap: true,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Shipping Address",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          " ",
+                          // snapshot.data!.billing['first_name'] + " " + snapshot.data!.billing['last_name'],
+                          style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          address,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          children: [
+                            FloatingActionButton.extended(
+                              heroTag: "delete",
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              backgroundColor: Colors.grey.shade100,
+                              label: const Icon(
+                                Icons.delete,
+                                size: 18,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {},
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            FloatingActionButton.extended(
+                              heroTag: "edit",
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              backgroundColor: Colors.grey.shade100,
+                              label: Icon(
+                                Icons.edit,
+                                size: 18,
+                                color: kPrimaryColor,
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (_) => const EditAddressPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    );
+                  }
+                },
               ),
             ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              children: [
-                FloatingActionButton.extended(
-                  heroTag: "delete",
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  backgroundColor: Colors.grey.shade100,
-                  label: const Icon(
-                    Icons.delete,
-                    size: 18,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {},
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                FloatingActionButton.extended(
-                  heroTag: "edit",
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  backgroundColor: Colors.grey.shade100,
-                  label: const Icon(
-                    Icons.edit,
-                    size: 18,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            )
           ],
         ),
       ),
