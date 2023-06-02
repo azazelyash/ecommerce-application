63eb7d739825a3b2682661e97dbb88eda94109eb
Yash Vishwakarma <jss.yash085@gmail.com>
2023-06-02 18:09:56 +0530

    Past Order Card

--- lib/pages/orders/orderspage.dart
+++ lib/pages/orders/orderspage.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:abhyukthafoods/models/customer.dart';
 import 'package:abhyukthafoods/models/order.dart';
 import 'package:abhyukthafoods/network/fetch_orders.dart';
@@ -20,49 +22,51 @@ class OrdersPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: CustomScrollView(slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                TextField(
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Search',
-                      hintStyle: const TextStyle(
-                        color: Colors.white,
-                      ),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                      filled: true,
-                      fillColor: Colors.black),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-          SliverFillRemaining(
-            child: FutureBuilder(
-              future: fetchOrders(customerModel!.id.toString()),
-              builder: (context, snapshot) => snapshot.connectionState == ConnectionState.waiting
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : !snapshot.hasData
-                      ? const Center(
-                          child: Text('No data'),
-                        )
-                      : ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) => OrderCard(order: snapshot.data![index]),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  TextField(
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
                         ),
+                        hintText: 'Search',
+                        hintStyle: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                        fillColor: Colors.black),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
             ),
-          )
-        ]),
+            SliverFillRemaining(
+              child: FutureBuilder(
+                future: fetchOrders(customerModel!.id.toString()),
+                builder: (context, snapshot) => snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : !snapshot.hasData
+                        ? const Center(
+                            child: Text('No data'),
+                          )
+                        : ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) => OrderCard(order: snapshot.data![index]),
+                          ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -71,102 +75,127 @@ class OrdersPage extends StatelessWidget {
 class OrderCard extends StatelessWidget {
   const OrderCard({super.key, required this.order});
   final Order order;
-  static final buttonRadius = BorderRadius.all(Radius.circular(10));
+  static const buttonRadius = BorderRadius.all(Radius.circular(10));
+
+  String formatDate(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    return '${dateTime.day}/${dateTime.month}/${dateTime.year} - ${dateTime.hour}:${dateTime.minute}';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: [
-          Container(
-            height: 70,
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: Padding(padding: const EdgeInsets.all(10), child: Text('Order No. ${order.id}'))),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                        borderRadius: buttonRadius,
-                        color: Theme.of(context).colorScheme.primary),
-                    child: Text(
-                      order.status[0].toUpperCase() + order.status.substring(1),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-
-                )
-              ],
-            ),
-          ),
-          ...order.itemList
-              .map(
-                (e) => Column(
+    log(order.dateCreated);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    ListTile(
-                      leading: Container(
-                        padding: const EdgeInsets.all(10),
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[100],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        // child: Image.asset('assets/Rectangle 33.png'),
-                      ),
-                      title: Text('${e.name}  x${e.quantity}', style: Theme.of(context).textTheme.titleSmall),
-                      trailing: Text('Rs ${e.total}'),
+                    Expanded(
+                      child: Text('Order No. ${order.id}'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: List.generate(
-                            90,
-                            (index) => Expanded(
-                                  child: Container(
-                                    color: index % 2 == 0 ? Colors.transparent : Colors.grey,
-                                    height: 1,
-                                  ),
-                                )),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: buttonRadius,
+                        color: Theme.of(context).colorScheme.primary,
                       ),
-                    ),
+                      child: Text(
+                        order.status[0].toUpperCase() + order.status.substring(1),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    )
                   ],
                 ),
-              )
-              .toList(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Expanded(child: Text('${order.dateCreated}')), Text('${order.total}')],
+              ),
             ),
-          ),
-          const Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Expanded(child: Text('Rate')),
-
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape:
-                            RoundedRectangleBorder(borderRadius: buttonRadius),
-                        backgroundColor: Colors.black),
-                    onPressed: () {},
-                    child: const Text('Reorder')),
-
-              ],
+            ...order.itemList
+                .map(
+                  (e) => Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          '${e.name} x ${e.quantity}',
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                        trailing: Text('Rs ${e.total}'),
+                      ),
+                      Row(
+                        children: List.generate(
+                          90,
+                          (index) => Expanded(
+                            child: Container(
+                              color: index % 2 == 0 ? Colors.transparent : Colors.grey,
+                              height: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+                .toList(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Text(
+                      formatDate(order.dateCreated),
+                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                  Text(
+                    "Rs. ${order.total}",
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+
+            /* ----------------------------- Reorder Button ----------------------------- */
+
+            // const Divider(),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //     children: [
+            //       const Expanded(child: Text('Rate')),
+            //       ElevatedButton(
+            //         style: ElevatedButton.styleFrom(
+            //           shape: RoundedRectangleBorder(borderRadius: buttonRadius),
+            //           backgroundColor: Colors.black,
+            //         ),
+            //         onPressed: () {},
+            //         child: const Text('Reorder'),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
